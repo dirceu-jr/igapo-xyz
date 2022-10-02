24c9e4eff216d65e96f13da2e756b3d92fe832eb
dirceupj <dirceuu@gmail.com>
2022-10-02 03:55:40 -0300

    Add Lodash

--- public/js/live-cursor.js
+++ public/js/live-cursor.js
@@ -4,7 +4,7 @@
 
 let
   _map = document.getElementById('map'),
-  available_colors = ['#62d2fd', '#3e63fb', '#807e97', '#fed861', '#febdbe', '#fd7384', '#f8cb75'],
+  available_colors = ['62d2fd', '3e63fb', '807e97', 'fed861', 'febdbe', 'fd7384', 'f8cb75'],
   this_color = available_colors[Math.floor(Math.random() * available_colors.length)]
   ;
 
@@ -21,7 +21,7 @@ function getOrCreateCursorFor(user_name, color) {
     ;
 
   cursor.setAttribute("data-sender", user_name);
-  svgPath.setAttribute('fill', `${this_color}`);
+  svgPath.setAttribute('fill', `#${this_color}`);
 
   document.body.insertBefore(cursor, _map);
 
@@ -39,30 +39,30 @@ document.addEventListener('DOMContentLoaded', function () {
     ;
 
   if (user_name !== undefined) {
-
     var user_ref = firebase.database().ref('users/' + user_name);
 
     // send
-    document.body.onmousemove = (evt) => {
+    document.body.onmousemove = _.throttle(function(e) {
       user_ref.set({
-        x: evt.clientX,
-        y: evt.clientY,
+        x: e.clientX,
+        y: e.clientY,
         c: this_color
       });
-    };
-
-    // receive
-    firebase.database().ref('/users').on('value', snapshot => {
-      const data = snapshot.val();
-
-      for (let item in data) {
-        // if different than this user
-        if (item !== user_name) {
-          // create or move cursor
-          const cursor = getOrCreateCursorFor(item, data[item].c);
-          cursor.style.transform = `translate(${data[item].x}px, ${data[item].y}px)`;
-        }
-      }
-    });
+    }, 60);
   }
+
+  // receive
+  firebase.database().ref('/users').on('value', snapshot => {
+    const data = snapshot.val();
+
+    for (let item in data) {
+      // if different than this user
+      if (item !== user_name) {
+        // create or move cursor
+        const cursor = getOrCreateCursorFor(item, data[item].c);
+        cursor.style.transform = `translate(${ data[item].x }px, ${ data[item].y }px)`;
+      }
+    }
+  });
+
 });
